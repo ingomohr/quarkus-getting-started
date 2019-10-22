9a2314752862ba034bf1c48c824c2480cfc11f4f
Ingo Mohr <tellastory73@gmail.com>
2019-10-22 11:09:25 +0200

    feat: add GreetingService via DI

--- getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -9,13 +9,14 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
+	@Test
+	void helloEndpoint() {
+		given().when().get("/hello").then().statusCode(200).body(is("Hello"));
+	}
+
+	@Test
+	void greetingsEndpoint() {
+		given().when().get("/hello/greeting/Joe").then().statusCode(200).body(is("Hello Joe"));
+	}
 
 }
